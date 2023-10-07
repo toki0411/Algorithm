33bed5081a50100032957a438b1e4ecff3acef71
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-10-07 19:16:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 99.80 ms, Memory: 14.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if stack and i == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(i)
+    return 0 if stack else 1
