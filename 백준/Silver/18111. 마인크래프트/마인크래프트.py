138b28cbf03b1db264aecbc3cce1b89cc3dd675a
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-10-14 17:39:58 +0900

    [Silver II] Title: 마인크래프트, Time: 808 ms, Memory: 112612 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,32 +1,22 @@
 import sys
+
 n, m, b = list(map(int, input().split()))
 graph = [list(map(int, input().split())) for _ in range(n)]
 idx = 0
 answer = sys.maxsize
 
-# 0층부터 256층까지 반복
 for floor in range(257):
-    max_target, min_target = 0, 0 # 블럭의 초과분, 부족분
+    max_block, min_block = 0, 0  # 초과분, 부족분
 
-    # 반복문을 통해 블록을 확인
     for i in range(n):
         for j in range(m):
+            if graph[i][j] >= floor:  # 초과분 계산
+                max_block += graph[i][j] - floor
+            else:  # 부족분 계산
+                min_block += floor - graph[i][j]
 
-            # 블록이 층수보다 더 크면
-            if graph[i][j] >= floor:
-                max_target += graph[i][j] - floor
-
-            # 블록이 층수보다 더 작으면
-            else:
-                min_target += floor - graph[i][j]
-
-    # 블록을 뺀 것과 원래 있던 블록의 합과 블록을 더한 값을 비교
-    # 블록을 뺀 것과 원래 있던 블록의 합이 더 커야 층을 만들 수 있음.
-    if max_target + b >= min_target:
-        # 시간 초를 구하고 최저 시간과 비교
-        if min_target + (max_target * 2) <= answer:
-            # 0부터 256층까지 비교하므로 업데이트 될 수록 고층의 최저시간
-            answer = min_target + (max_target * 2) #최저 시간
-            idx = floor # 층수
+    if (max_block + b) >= min_block:  # floor층 개수로 평탄하게 할 수 있는지 체크(초과분 + 인벤토리의 블럭 수가 부족분보다 많으면 평탄하게 가능)
+        if max_block * 2 + min_block <= answer:
+            answer = max_block * 2 + min_block
+            idx = floor  # 층수
 print(answer, idx)
-
