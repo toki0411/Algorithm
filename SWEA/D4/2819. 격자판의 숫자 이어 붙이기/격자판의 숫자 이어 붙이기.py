49dc01f0318abd323bbb2542130e16f4146e1317
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-02 14:37:54 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 1,439 ms, Memory: 78,080 KB -BaekjoonHub

---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -0,0 +1,22 @@
+
+t = int(input())
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+def dfs(x, y, num):
+    if len(num) > 7:
+        return
+    if len(num) == 7 and ''.join(num) not in ans:
+        ans.add(''.join(num))
+        return
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            dfs(nx, ny, num+[str(graph[nx][ny])])
+for tc in range(1, t + 1):
+    graph = [list(map(int, input().split())) for _ in range(4)]
+    ans = set(); num = []; visited = [[0]*4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            dfs(i, j, num)
+    print('#{} {}'.format(tc, len(ans)))
