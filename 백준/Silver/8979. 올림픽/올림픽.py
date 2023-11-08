77500a46d123b9d9681d55ec969bee1c31e8fa43
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-08 17:24:28 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,13 @@
 n, k = map(int, input().split())
 score = [list(map(int, input().split())) for _ in range(n)]
 score.sort(key = lambda x:(-x[1], -x[2], -x[3]))
-
-prev_gold = prev_silver = prev_bronze = -1; dict = {}; cnt = 0; rank = 1
+target = 0
 for i in range(len(score)):
-    gold , silver , bronze = score[i][1], score[i][2], score[i][3]
-    idx = score[i][0]
-    if prev_gold ==gold and prev_silver == silver and prev_bronze == bronze:
-        cnt += 1
-        dict[idx] = dict[prev_idx]
-    else:
-        if cnt > 1: #앞에 중복이 있을 경우
-            rank -=1
-            dict[idx] = cnt + rank
-        else:
-            dict[idx] = rank
-            rank += 1
-        cnt = 1
-    prev_idx,prev_gold, prev_silver, prev_bronze = score[i][0],score[i][1], score[i][2], score[i][3]
+    if score[i][0] == k:
+        target = score[i][1:]
+        break
+for i in range(len(score)):
+    if target == score[i][1:]:
+        print(i+1)
+        break
 
-print(dict[k])
