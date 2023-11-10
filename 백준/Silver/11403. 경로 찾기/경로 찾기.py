5ec3f335f8034119f5acb389a3fca9f06e3ed988
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-10 18:50:13 +0900

    [Silver I] Title: 경로 찾기, Time: 80 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -3,19 +3,18 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 graph = [[] for _ in range(n)]
 ans = [[0] * n for _ in range(n)]
-# visited = []* n
+
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
             graph[i].append(j)
+def dfs(v, x):
+    for i in graph[x]:
+        if ans[v][i] == 0:
+            ans[v][i] = 1
+            dfs(v, i)
 
 for i in range(n):
-    q = deque([i])
-    while q:
-        v = q.popleft()
-        for j in graph[v]:
-            if ans[i][j] == 0:
-                ans[i][j] = 1
-                q.append(j)
-for i in range(n):
+    dfs(i,i)
     print(*ans[i])
+
