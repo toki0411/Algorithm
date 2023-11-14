86a848e0bda6593f78058da47352189ad32fb43a
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-15 01:27:38 +0900

    [Gold V] Title: 퇴사 2, Time: 1332 ms, Memory: 287804 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,10 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [0] * (N+1)
+for i in range(N-1,-1,-1):
+    if arr[i][0] + i > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], arr[i][1]+dp[i+arr[i][0]])
+
+print(dp[0])
