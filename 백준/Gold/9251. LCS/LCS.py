5ae587c165027acbae87866e1052566f3b9877db
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-14 20:42:38 +0900

    [Gold V] Title: LCS, Time: 536 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,11 @@
+s1 = ' ' + input()
+s2 = ' ' + input()
+dp = [[0] * len(s1) for _ in range(len(s2))]
+
+for i in range(1, len(s2)):
+    for j in range(1, len(s1)):
+        if s1[j] == s2[i] :
+            dp[i][j] = dp[i-1][j-1] + 1
+        else :
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
