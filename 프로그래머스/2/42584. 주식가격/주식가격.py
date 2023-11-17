8045ad990d46cd77b7a00bd9277353c2e1d5182a
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-17 17:12:15 +0900

    [level 2] Title: 주식가격, Time: 35.92 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,9 +1,16 @@
-from bisect import bisect_right
+from collections import deque
+
+
 def solution(prices):
-    ans = [0 for _ in range(len(prices))]
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            ans[i] +=1
-            if prices[i] > prices[j] :
+    answer = []
+    q = deque(prices)
+    while q:
+        x = q.popleft()
+        seq = 0
+        for i in q:
+            seq += 1
+            if x > i:
                 break
-    return ans
+        answer.append(seq)
+    return answer
+print(solution([1,2,3,2,3]))
