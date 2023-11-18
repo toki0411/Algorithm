24b99d9d1493b0dc257b637f8279100eed68175f
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-11-18 15:03:43 +0900

    [Gold V] Title: LCS, Time: 504 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,11 +1,12 @@
-s1 = ' ' + input()
-s2 = ' ' + input()
-dp = [[0] * len(s1) for _ in range(len(s2))]
+a = [' ']+list(input())
+b = [' ']+list(input())
 
-for i in range(1, len(s2)):
-    for j in range(1, len(s1)):
-        if s1[j] == s2[i] :
+dp = [[0] * (len(b)) for _ in range(len(a))]
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
             dp[i][j] = dp[i-1][j-1] + 1
-        else :
+        else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-print(dp[-1][-1])
+
+print(dp[len(a)-1][len(b)-1])
