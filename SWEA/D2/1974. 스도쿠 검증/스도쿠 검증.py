e37011a01305ba3c51a0b36e8c924112838d45bf
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-01-13 21:33:35 +0900

    [D2] Title: 스도쿠 검증, Time: 158 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,46 +1,37 @@
 t = int(input())
-for tc in range(1, t+1):
+index_arr = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
+for t_ in range(1, t + 1):
+    ans = 1
     graph = [list(map(int, input().split())) for _ in range(9)]
-    reversed_graph = graph[::-1]
-    rotated = list(zip(*reversed_graph))
-    check = [0] * 10
-    ans = 0; key = False
-    index_arr = [(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
+    #가로 체크
     for i in range(9):
-        check = [0] * 10
-        arr = graph[i]
+        visited = [0] * 10
         for j in range(9):
-            if check[arr[j]] == 1:
-                key = True
+            if visited[graph[i][j]] == 1:
+                ans = 0
                 break
             else:
-                check[arr[j]] = 1
-
-        check = [0] * 10
-        arr2 = rotated[i]
+                visited[graph[i][j]] = 1
+        # 세로 체크
+        visited = [0] * 10
         for j in range(9):
-            if check[arr2[j]] == 1:
-                key = True
+            if visited[graph[j][i]] == 1:
+                ans = 0
                 break
             else:
-                check[arr2[j]] = 1
+                visited[graph[j][i]] = 1
 
-        check = [0] * 10
+        visited = [0] * 10
         x, y = index_arr[i]
         for j in range(x, x+3):
             for k in range(y, y+3):
-                if check[graph[j][k]] == 1:
-                    key = True
+                if visited[graph[j][k]] == 1:
+                    ans = 0
                     break
                 else:
-                    check[graph[j][k]] = 1
-            if key:
-                break
-        if key:
+                    visited[graph[j][k]] = 1
+        if ans == 0:
             break
-    if key:
-        ans = 0
-    else:
-        ans = 1
-    print('#{} {}'.format(tc,ans))
 
+
+    print('#{} {}'.format(t_, ans))
