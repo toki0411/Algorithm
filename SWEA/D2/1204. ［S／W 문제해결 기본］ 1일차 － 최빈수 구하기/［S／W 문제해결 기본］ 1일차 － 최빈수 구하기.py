2dc9d1ef2d1731007194cdf6c88b4d8d06acfae9
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-01-14 19:30:16 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 165 ms, Memory: 62,020 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,14 +1,13 @@
 t = int(input())
-for tc in range(1, t+1):
-    tt = int(input())
-    number = list(map(int, input().split()))
+for t_ in range(1, t + 1):
+    int(input())
     dict = {}
-    for num in number:
-        if num in dict:
-            dict[num]+=1
+    arr = list(map(int, input().split()))
+    for num in arr:
+        if dict.get(num):
+            dict[num] += 1
         else:
             dict[num] = 1
-    ans = sorted(dict.items(), reverse=True)
-    ans.sort(key = lambda x:-x[1])
 
-    print('#{} {}'.format(tt, ans[0][0]))
+    sorted_dict = sorted(dict.items(), key = lambda item: -item[1])
+    print('#{} {}'.format(t_, sorted_dict[0][0]))
