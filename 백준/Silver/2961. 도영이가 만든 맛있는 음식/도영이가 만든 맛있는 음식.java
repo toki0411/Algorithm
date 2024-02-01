90c4c045734063eb052c873c354e2ee30445a4b5
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-02-01 13:44:10 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -10,8 +10,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		sour = new int [n];
-		bitter = new int [n];
+		sour = new int [10];
+		bitter = new int [10];
 		for (int i = 0 ; i< n; i++) {
 			st = new StringTokenizer(br.readLine());
 			sour[i] = Integer.parseInt(st.nextToken());
@@ -24,18 +24,17 @@ public class Main {
 		int cnt= 1 << n;  // 1<<n : 2^n 부분집합의 개수 
 		int sour_v = 0; int bitter_v = 0;
 		int ans = Math.abs(sour[0]- bitter[0]);
-		boolean key = false;
-		for (int i =0; i<cnt; ++i) {
-			sour_v = 1; bitter_v = 0; key = false;
-			for (int position = 0; position < n; ++position) {  //부분집합 하나마다 각 위치의 원소들이 부분집합에 
+		for (int i =1; i<cnt; i++) {
+			sour_v = 1; bitter_v = 0; 
+			for (int position = 0; position < n; position++) {  //부분집합 하나마다 각 위치의 원소들이 부분집합에 
 				// i의 이진표현 값에서 position 위치 값이 1인지 0인지 체크 
 				if ((i & 1 << position) != 0) {
-					key = true;
 					sour_v = sour_v * sour[position];
 					bitter_v += bitter[position];
 				}
 			}
-			if (Math.abs(sour_v - bitter_v) < ans && key) {
+			
+			if (Math.abs(sour_v - bitter_v) < ans ) {
 				ans = Math.abs(sour_v - bitter_v);
 			}
 		}
