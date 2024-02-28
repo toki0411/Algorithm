6134810fb5628fedcd25a21caa9dd3b1985d2541
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-02-28 21:29:10 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,50 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int graph[][];
-	static int dp[][];
-	static int ans;
+    static int N;
+    static int graph[][];
+    static int dp[][][];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		graph = new int[N][N];
-		dp = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		ans = 0;
-		dfs(0,1,0);
-		System.out.println(ans);
-	}
-	private static void dfs(int x, int y, int pos) {
-		if (x == N-1 && y == N-1) {
-			ans++;
-			return;
-		}
-		if (pos == 0 || pos == 2) {  //가로
-			int ny = y + 1;
-			if (ny < N && graph[x][ny] == 0) 
-				dfs(x, y+1, 0);
-		}
-		if (pos == 1 || pos == 2) { //세로 
-			int nx = x + 1;
-			if (nx < N && graph[nx][y] == 0) 
-				dfs(x+1, y, 1);
-		}
-		//대각선
-		if (x + 1 < N && y + 1 < N && graph[x+1][y+1] == 0 && graph[x+1][y] == 0 && graph[x][y+1]==0) {
-			dfs(x+1, y+1, 2);
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        graph = new int[N][N];
+        dp = new int[N][N][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][1][0] = 1;
+        for (int i = 2; i < N; i++) {
+            if (graph[0][i] == 0)
+                dp[0][i][0] = dp[0][i-1][0];
+        }
+        System.out.println(dp());
+        //System.out.println(Arrays.deepToString(dp));
+
+    }
+    private static int dp() {
+        for (int i = 1; i < N; i++) {
+            for (int j = 2; j < N; j++) {
+                if (graph[i][j] == 1) continue;
+                dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+                dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+                if (graph[i-1][j] ==0 && graph[i][j-1] == 0)
+                    dp[i][j][2] = dp[i-1][j-1][0]+ dp[i-1][j-1][1] + dp[i-1][j-1][2];
+            }
+        }
+
+        return dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2];
+    }
 
 }
