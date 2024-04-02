8a9b3cd07dad182a08f6166974ff6671477f5fda
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-03 00:27:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 266 ms, Memory: 72,080 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,48 +1,57 @@
-from collections import deque
+import copy
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def dfs(x, y, k, cnt):
+    # print(x, y, k, cnt)
+    global ans
+    ans = max(ans, cnt)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= N or ny >= N:
+            continue
+        if visited[nx][ny] : continue
+        if graph[nx][ny] < graph[x][y]:
+            visited[nx][ny] = 1
+            dfs(nx, ny, k, cnt + 1)
+            visited[nx][ny] = 0
+        elif graph[nx][ny] - K < graph[x][y] and k == 1 : #등산로를 깎는다.
+            tmp = graph[nx][ny]
+            while graph[nx][ny] >= graph[x][y]:
+                graph[nx][ny] -= 1
+            if graph[nx][ny] < 0 : graph[nx][ny] = 0
+            visited[nx][ny] = 1
+            dfs(nx, ny, 0, cnt + 1)
+            visited[nx][ny] = 0
+            graph[nx][ny] = tmp
 
-
-def bfs(x, y):
-    val = 0
-    q = deque()
-    q.append((x, y, 1))
-
-    while q:
-        x, y, d = q.popleft()
-        val = max(d, val)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n: continue
-            if graph[nx][ny] < graph[x][y]:
-                q.append((nx, ny, d+1))
-
-    return val
 
 T = int(input())
-for tc in range(1, T + 1):
-    ans = 0
-    n, K = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(n)]
-    max_val = max(map(max, graph))
-    maximum = []
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == max_val:
-                maximum.append((i, j))
 
-    for k in range(K+1):
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] - k < 0:
-                    continue
-                else:
-                    graph[i][j] -= k
-                for l in range(len(maximum)):
-                    v= bfs(maximum[l][0], maximum[l][1])
-                    ans = max(ans, v)
-                graph[i][j] += k
+for tc in range(1, T+1):
+    ans = 1
+    N, K = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    graph2 = [[0] * N for _ in range(N)]
 
-    print('#{} {}'.format(tc, ans))
+    maxVal = 0
+    for i in range(N):
+        maxVal = max(max(graph[i]), maxVal)
+        for j in range(N):
+            graph2[i][j] = graph[i][j]
+
+    visited = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == maxVal:
+                visited = [[0] * N for _ in range(N)]
+                visited[i][j] = 1
+                dfs(i, j, 1, 1)
+                for l in range(N):
+                    for r in range(N):
+                        graph[l][r] = graph2[l][r]
+                for v in visited:
+                    ans = max(max(v), ans)
+
+    print("#{} {}".format(tc, ans))
