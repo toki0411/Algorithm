0a32ff6cb2c34c0ef29c8a31f57f4e19475c5297
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-02 11:19:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 8,520 ms, Memory: 150,848 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
@@ -1,49 +1,49 @@
 import copy
-def dfs(cnt, col):
-    global ans
-    if check():
-        ans = min(ans, cnt)
-        return
-    if cnt >= ans:
-        return
-    for i in range(col, d):
-        origin = copy.deepcopy(graph[i])
-        # i번째를 0으로
-        for r in range(w):
-            graph[i][r] = 0
-        dfs(cnt + 1, i + 1)
-
-        # i번째를 1으로
-        for r in range(w):
-            graph[i][r] = 1
-        dfs(cnt + 1, i + 1)
-
-        # 그래프 원래대로
-        for r in range(w):
-            graph[i][r] = origin[r]
-def check():
-    for j in range(w):
-        cnt = 1
-        for i in range(1, d):
-            if graph[i][j] == graph[i-1][j]:
-                cnt += 1
-            else:
-                cnt = 1
-            if cnt >= k:
-                break
-        if cnt < k:
-            return False
-    # print(graph)
-    return True
 
 T = int(input())
-for tc in range(1, T + 1):
-    d, w, k = map(int, input().split())  # 필름의 두께, 가로 크기, 합격 기준
-    graph = [list(map(int, input().split())) for _ in range(d)]
-    # graph2 = copy.deepcopy(graph)
-    ans = d + 1
-    if check() or k == 1:
-        print('#{} {}'.format(tc, 0))
+def dfs(depth, start):
+    global ans
+    if check():
+        ans = min(depth, ans)
+        return
+    if depth >= ans:
+        return
+    for i in range(start, N):
+        tmp = copy.deepcopy(graph[i])
+
+        for j in range(M):
+            graph[i][j] = 1
+        dfs(depth + 1, i + 1)
+
+        for j in range(M):
+            graph[i][j] = 0
+        dfs(depth + 1, i + 1)
+
+        for j in range(M): #그래프 복구
+            graph[i][j] = tmp[j]
+
+def check():
+    for i in range(M):
+        tmp = -1
+        cnt = 1
+        for j in range(N):
+            if tmp != graph[j][i]:
+                tmp = graph[j][i]
+                cnt = 1
+            elif tmp == graph[j][i]:
+                cnt += 1
+            if cnt >= K:
+                break
+        if cnt < K:
+            return False
+    return True
+
+for tc in range(1, T+1):
+    N, M, K = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    ans = N
+    if check() or K == 1:
+        print("#{} {}".format(tc, 0))
     else:
         dfs(0, 0)
-        print('#{} {}'.format(tc, ans))
+        print("#{} {}".format(tc, ans))
