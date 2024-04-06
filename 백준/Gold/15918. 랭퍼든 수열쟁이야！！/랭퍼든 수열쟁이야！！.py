de275a504fc13b9fbc1d0779a221ec976f5053e8
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-06 23:07:12 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 596 ms, Memory: 117748 KB -BaekjoonHub

--- 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.py
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.py
@@ -1,25 +1,28 @@
 n, x, y = map(int, input().split())
-total = 0
-
-
-def dfs(depth):
-    global total
-    if depth == (y - x - 1):
-        dfs(depth + 1)
-    if depth == n + 1:
-        total += 1
+def dfs(cnt):
+    global s
+    if cnt == (2*n+1):
+        s += 1
         return
-    for i in range(1, len(ans) - depth - 1):
-        if ans[i] == 0 and ans[i + depth + 1] == 0:
-            ans[i] = depth
-            ans[i + depth + 1] = depth
-            dfs(depth + 1)
-            ans[i] = 0
-            ans[i + depth + 1] = 0
+    if ans[cnt] == 0:
+        for i in range(1, n+1):
+            if num[i] > 0 and cnt + 1 + i < 2*n + 1:
+                if ans[cnt+ 1 + i] == 0:
+                    ans[cnt] = i
+                    ans[cnt + 1 + i] = i
+                    num[i] -= 2
+                    dfs(cnt + 1)
+                    num[i] += 2
+                    ans[cnt] = 0
+                    ans[cnt + 1 + i] = 0
 
-
-ans = [0] * (2 * n + 1)
-ans[y] = y - x - 1
-ans[x] = y - x - 1
+    else:
+        dfs(cnt + 1)
+s = 0
+ans = [0] * (2*n+1)
+ans[x] = y-x-1
+ans[y] = y-x-1
+num = [0] + [2] * (n)
+num[y-x-1] -= 2
 dfs(1)
-print(total)
+print(s)
