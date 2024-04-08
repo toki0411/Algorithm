15e3a073c6d11c99f4562df7a982943e47f374cd
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-08 17:57:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 530 ms, Memory: 57,844 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
@@ -1,52 +1,70 @@
 
-T = int(input())
 
+def priceMeasure():
+    A = []
+    B = []
+    for i in range(M):
+        A.append(board[isSelected[0][0]][isSelected[0][1] + i])
+        B.append(board[isSelected[1][0]][isSelected[1][1] + i])
+    val = 0
+    def subset(H, cnt, selected):
+        nonlocal val
+        if cnt == len(H):
+            total = 0
+            tmp = 0
+            for i in range(len(H)):
+                if selected[i]:
+                    total += H[i]
+                    tmp += H[i] * H[i]
+            if total > C: return
+            val = max(val, tmp)
+            return
 
-def subset(cnt, tmp):
-    global maxVal
-    if cnt == m:
-        a = 0
-        for i in range(m):
-            if ans[i] == 1:
-                a+= tmp[i]
-        if a > c: return
-        val = 0
-        for i in range(m):
-            if ans[i] == 1:
-                val += (tmp[i]*tmp[i])
+        selected[cnt] = 1
+        subset(H, cnt + 1, selected)
+        selected[cnt] = 0
+        subset(H, cnt + 1, selected)
 
-        maxVal = max(val, maxVal)
+    selected = [0] * len(A);
+    subset(A, 0, selected)
+    aHoney = val
+
+    selected = [0] * len(B)
+    val = 0
+    subset(B, 0, selected)
+    bHoney = val
+
+    return aHoney + bHoney
+
+def dfs(cnt, x):
+    global ans
+    if cnt == 2:
+        # print(isSelected)
+        price = priceMeasure()
+        ans = max(price, ans)
         return
-    ans[cnt] = 1
-    subset(cnt+1, tmp)
-    ans[cnt] = 0
-    subset(cnt+1, tmp)
+    # y idx 조정
+    # if y + M > N:
+    #     x += 1
+    #     y = 0
+    for i in range(x, N):
+        for j in range(N-M+1):
+            if not visited[i][j]:
+                for k in range(M):
+                    visited[i][j+k] = 1
+                isSelected[cnt] = [i, j]
+                dfs(cnt + 1, i+1)
+                for k in range(M):
+                    visited[i][j + k] = 0
 
-for tc in range(1, T + 1):
-    n,m,c = map(int, input().split())
-    honey = []
-    graph = [list(map(int, input().split())) for _ in range(n)]
-    for i in range(n):
-        for j in range(n-m+1):
-            tmp = graph[i][j:j+m]
-            ans = [0] * m
-            if sum(tmp) > c:
-                maxVal = 0
-                subset(0, tmp)
-                honey.append((maxVal, i, j))
-            else:
-                val = 0
-                for l in range(len(tmp)):
-                    val += (tmp[l]*tmp[l])
-                honey.append((val, i, j))
-    honey.sort(key = lambda x : -x[0])
-    # print(honey)
-    total_honey = honey[0][0]
-    x = honey[0][1]
-    # print(honey)
-    for i in range(1,len(honey)):
-        if x != honey[i][1]:
-            total_honey += honey[i][0]
-            break
+T = int(input())
+for tc in range(1, T+1):
+    N, M, C = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
+    isSelected = [0] * (2)
+    visited = [[0] * N for _ in range(N)]
 
-    print('#{} {}'.format(tc, total_honey))
+    selected = [0]
+    dfs(0, 0)
+    print("#{} {}".format(tc, ans))
