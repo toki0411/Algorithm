f22febb0619f33282e2bb6011948f0ce3dd7d8ce
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-10 18:14:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 3,710 ms, Memory: 66,860 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
@@ -1,49 +1,48 @@
-import copy
-
-T = int(input())
-def dfs(depth, start):
+def subset(start, depth):  #약품을 투입하는 모든 경우를 찾음. 현재 인덱스, 약품 투입 횟수
     global ans
+    if depth > ans:
+        return
     if check():
-        ans = min(depth, ans)
+        ans = min(ans, depth)
         return
-    if depth >= ans:
+    if start >= N:
         return
+
     for i in range(start, N):
-        tmp = copy.deepcopy(graph[i])
-
+        graph2 = [0] * M
         for j in range(M):
+            graph2[j] = graph[i][j]
+        #약품 처리
+        for j in range(M):  #A로
             graph[i][j] = 1
-        dfs(depth + 1, i + 1)
+        subset(i + 1, depth + 1)
+
+        for j in range(M):  #B로
+            graph[i][j] = 0
+        subset(i + 1, depth + 1)
 
         for j in range(M):
-            graph[i][j] = 0
-        dfs(depth + 1, i + 1)
-
-        for j in range(M): #그래프 복구
-            graph[i][j] = tmp[j]
+            graph[i][j] = graph2[j]
 
 def check():
     for i in range(M):
-        tmp = -1
-        cnt = 1
-        for j in range(N):
-            if tmp != graph[j][i]:
-                tmp = graph[j][i]
-                cnt = 1
-            elif tmp == graph[j][i]:
+        cnt = 1;
+        for j in range(N-1):
+            if graph[j][i] == graph[j+1][i]:
                 cnt += 1
+            else:
+                cnt = 1
             if cnt >= K:
                 break
         if cnt < K:
             return False
     return True
 
+
+T =int(input())
 for tc in range(1, T+1):
+    ans = 1e9
     N, M, K = map(int, input().split())
     graph = [list(map(int, input().split())) for _ in range(N)]
-    ans = N
-    if check() or K == 1:
-        print("#{} {}".format(tc, 0))
-    else:
-        dfs(0, 0)
-        print("#{} {}".format(tc, ans))
+    subset(0,0)
+    print('#{} {}'.format(tc, ans))
