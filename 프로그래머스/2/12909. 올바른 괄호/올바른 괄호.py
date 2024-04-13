097ccc0ba8a780590f0ef8b11a651a60b7ff9138
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-13 16:10:24 +0900

    [level 2] Title: 올바른 괄호, Time: 9.57 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(str):
+    stack = []
+    for s in str:
+        if s == '(':
+            stack.append('(')
+        elif s != '(' and len(stack) != 0:
+            stack.pop()
+        else:
+            return False
+    if len(stack): return False
+    return True
