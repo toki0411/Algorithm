83c4776a534dab17e9b6792c3388ad9dfbbacda4
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-04-17 17:47:18 +0900

    [Silver I] Title: 점프, Time: 120 ms, Memory: 111608 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,15 +1,24 @@
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-ans = 0; dp = [[0] * n for _ in range(n)]
-dp[0][0] =1
-for i in range(n):
-    for j in range(n):
-        if i == n-1 and j == n-1:
-            print(dp[n-1][n-1])
-            break
-        down_x = i + board[i][j]
-        right_y = j + board[i][j]
-        if 0 <= down_x < n:
-            dp[down_x][j] += dp[i][j]
-        if 0<= right_y < n:
-            dp[i][right_y] += dp[i][j]
+import sys
+sys.stdin.readline
+dx = [0,1]
+dy = [1,0]
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+dp = [[-1] * N for _ in range(N)]
+
+def dfs(x, y):
+    if x == N-1 and y == N-1:
+        return 1
+    if dp[x][y] != -1:
+        return dp[x][y]
+    else:
+        dp[x][y] = 0
+        for i in range(2):
+            nx = x + dx[i] * graph[x][y]
+            ny = y + dy[i] * graph[x][y]
+            if 0 <= nx < N and 0 <= ny <N:
+                dp[x][y] += dfs(nx, ny)
+    return dp[x][y]
+
+dfs(0,0)
+print(dp[0][0])
