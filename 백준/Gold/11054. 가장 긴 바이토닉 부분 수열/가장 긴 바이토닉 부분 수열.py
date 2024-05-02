ed630df263f32ed3e8f1ccb81c114c356180deef
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-05-02 09:23:55 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 140 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,17 +1,18 @@
 n = int(input())
 num = list(map(int, input().split()))
 dp = [1] * n
+dp2 = [1] * n
 for i in range(n):
     for j in range(i):
-        if num[i] > num[j]:
+        if num[j] < num[i]:
             dp[i] = max(dp[j]+1, dp[i])
-dp2 = [1] * n
+
 for i in range(n-1, -1, -1):
     for j in range(n-1, i, -1):
-        if num[i] > num[j]:
+        if num[j] < num[i]:
             dp2[i] = max(dp2[j]+1, dp2[i])
 
-ans =0
+ans = 0
 for i in range(n):
     ans = max(dp[i] + dp2[i] - 1, ans)
 print(ans)
