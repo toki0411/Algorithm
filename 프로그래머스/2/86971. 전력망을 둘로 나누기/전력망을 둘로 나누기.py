da73ff222432b34975bc482c3b2eaabc5d87b5d9
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-05-03 19:54:05 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,40 +1,31 @@
-def findParents(x):
-    global parents
-    if x == parents[x]:
-        return x
-    parents[x] = findParents(parents[x])
-    return parents[x]
+def dfs(now):
+    global d, visited, graph
+    d += 1
+    visited[now] = 1
+    for g in graph[now]:
+        if not visited[g]:
+            dfs(g)
 
-def union(a, b):
-    global parents
-    aRoot = findParents(a)
-    bRoot = findParents(b)
-    if aRoot < bRoot:
-        parents[bRoot] = aRoot
-    else:
-        parents[aRoot] = bRoot
-        
 def solution(n, wires):
+    global d, visited, graph
     wires.sort()
-    global parents
+    
     answer = 1e9
     for i in range(n-1):
-        parents = [i for i in range(n+1)]
+        graph = [[] for _ in range(n+1)]
         for j in range(n-1):
             if i != j:
                 a, b = wires[j][0], wires[j][1]
-                union(a,b)
-  
-        
-        diff = {}
-        for i in range(1, n+1):
-            p = findParents(parents[i])
-            if p in diff:
-                diff[p] += 1
-            else:
-                diff[p] = 1
-        diff = list(diff.values())
-    
+                graph[a].append(b)
+                graph[b].append(a)
+        visited = [0] * (n+1)
+        diff = []
+        for j in range(1, n+1):
+            if not visited[j]:
+                d = 0
+                dfs(j)
+                diff.append(d)
+        # print(diff)
         if len(diff) == 2:
-            answer = min(abs(diff[0]-diff[1]), answer)
+            answer = min(abs(diff[0] - diff[1]), answer)
     return answer
