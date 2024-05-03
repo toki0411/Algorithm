34e36b20e9488349586a99bebadf013e82fae617
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-05-03 11:52:13 +0900

    [Gold IV] Title: LCS 2, Time: 604 ms, Memory: 480088 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,19 +1,19 @@
-n = list(' '+input())
-m = list(' '+input())
-dp = [[""] * len(m) for _ in range(len(n))];
-for i in range(1, len(n)):
-    for j in range(1, len(m)):
-        if n[i] == m[j]:
-            dp[i][j] = dp[i-1][j-1]+n[i]
+s1 = [""]+list(input())
+s2 = [""]+list(input())
+dp = [['' for _ in range(len(s2))] for _ in range(len(s1))]
+for i in range(1, len(s1)):
+    for j in range(1, len(s2)):
+        if s1[i] == s2[j]:
+            dp[i][j] = dp[i-1][j-1] + s1[i]
+
         else:
-            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+            if len(dp[i-1][j]) > len(dp[i][j-1]):
                 dp[i][j] = dp[i-1][j]
             else:
                 dp[i][j] = dp[i][j-1]
-
+# print(dp)
 if len(dp[-1][-1]) == 0:
     print(0)
 else:
     print(len(dp[-1][-1]))
     print(dp[-1][-1])
-
