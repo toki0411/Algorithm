d0ed738907d73e47cb2d4463c4cc4c43dc90f2a6
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2024-06-14 13:15:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 7.72 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.05 ms
+메모리: 7.72 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 22:17:53
+2024년 06월 14일 13:15:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,23 +1,19 @@
 from collections import deque
-
-
 def solution(bridge_length, weight, truck_weights):
     time = 0
-    current_w = 0
+    now = 0
+    current_weight = 0
     q = deque()
-
-    while len(truck_weights) > 0 or q:
-        if q:
-            truck = q.popleft()
-            if time == truck[1]:
-                current_w -= truck[0]
-            else:
-                q.appendleft(truck)
-        if truck_weights :
-            if current_w + truck_weights[0] <= weight:
-                q.append([truck_weights[0], time + bridge_length])
-                current_w += truck_weights[0]
-                del truck_weights[0]
-
+    while True:
+        if now >= len(truck_weights) and len(q) == 0 :
+            break
         time += 1
+        if len(q) > 0 and q[0][1] == time:
+            v = q.popleft()
+            current_weight -= v[0]
+        if now < len(truck_weights):
+            if current_weight + truck_weights[now] <=weight:  # 추가
+                q.append((truck_weights[now], time + bridge_length))
+                current_weight += truck_weights[now]
+                now += 1
     return time
