dde0bdd576217795bbe8a2f80b2ffb1a88b6ea0d
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2025-03-20 23:21:19 +0900

    [Gold V] Title: 0 만들기, Time: 196 ms, Memory: 114088 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
-def dfs(n, s, num):
+global n;
+def dfs(s, num):
     if num == n :
         if eval(s.replace(' ', '')) == 0:
             print(s)
         return
 
-    dfs(n, s + ' ' + str(num+1), num + 1)
-    dfs(n, s + '+' + str(num + 1), num + 1)
-    dfs(n, s + '-' + str(num + 1), num + 1)
+    dfs(s + ' ' + str(num+1), num + 1)
+    dfs(s + '+' + str(num + 1), num + 1)
+    dfs(s + '-' + str(num + 1), num + 1)
 
 T = int(input())
 for tc in range(T):
     n = int(input())
-    dfs(n, "1", 1)
+    dfs( "1", 1)
     print()
