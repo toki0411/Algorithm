85b9b54851f7dc92129ac6931105e2846c80b33f
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2025-09-21 17:55:00 +0900

    [level 3] Title: 단어 변환, Time: 0.54 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,33 +1,30 @@
-def dfs(word, target, cnt, words):
-    global answer, visited
-    if word == target:
-        answer = min(answer, cnt)
-        return 
-    if cnt > answer:
+answer = 1e9
+def dfs(begin, target, words, cnt, visited):  #백트래킹
+    global answer 
+    if begin == target:
+        answer = min(cnt, answer)
         return 
     for i in range(len(words)):
-        if changeable(word, words[i]) and not visited[i]:
-            visited[i] = 1
-            dfs(words[i], target, cnt + 1, words)
-            visited[i] = 0
+        if not visited[i]:
+            if changeable(begin, words[i]):
+                visited[i] = 1
+                dfs(words[i], target, words, cnt + 1, visited)
+                visited[i] = 0
 
-            
-def changeable(word, target):
-    diff = 0
-    for i in range(len(word)):
-        if word[i] != target[i]:
-            diff += 1
-    if diff == 1:
+#변경할 수 있는 단어인지 확인                 
+def changeable(begin, target):
+    diffCnt = 0 
+    for i in range(len(begin)):
+        if begin[i] != target[i]:
+            diffCnt +=1
+    
+    if diffCnt == 1:
         return True
     else:
         return False
-        
-def solution(begin, target, words):
-    global answer, visited
-    answer = 1e9
-    if target not in words:
-        return 0
-    visited = [0] * len(words)
-    dfs(begin, target, 0, words)
     
-    return answer
+def solution(begin, target, words):
+    visited = [0] * len(words)
+    dfs(begin, target, words, 0, visited)
+    if answer == 1e9 : return 0
+    else : return answer
