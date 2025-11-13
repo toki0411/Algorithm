82c29820f19ff4fc2cb305fc6ad9d0e766a02011
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2025-11-13 22:00:38 +0900

    [level 2] Title: 디펜스 게임, Time: 111.81 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue <Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i =0 ; i< enemy.length; i++) {
+            n -= enemy[i];
+            pq.offer(enemy[i]);
+            if (n < 0) {
+                if (k > 0){
+                    n += pq.poll();
+                    k-=1;
+                }
+                else {
+                    break;
+                }
+            }
+
+            answer ++;
+        }
+        return answer;
+    }
+}
