5a1b1148e95c5113d543a9689cfa271f91663325
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2025-11-21 18:15:28 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    static HashMap<String, Integer> map ;
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        map = new HashMap<>();
+        for (String[] cloth : clothes){
+            if (!map.containsKey(cloth[1])){
+                map.put(cloth[1], 1);    
+            } else {
+                map.put(cloth[1], map.get(cloth[1])+1);
+            }   
+            
+        }
+        
+        for (String key: map.keySet()){
+            answer *= (map.get(key) + 1);
+        }
+        return answer - 1;
+    }
+}
